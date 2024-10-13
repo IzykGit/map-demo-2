c3b22a186ae0845382ae0ebaaacfab5fb7bfc412
Lance Hemphill <lance_hemphill1234@yahoo.com>
2024-10-12 20:56:08 -0400

    finished demo

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,12 +1,72 @@
-import MapStyles from '../styles/components/Map.module.css'
+import axios from "axios";
+
+import MapStyles from "../styles/components/Map.module.css";
+import { useEffect, useState } from "react";
 
 const Map = () => {
+  const [countries, setCountries] = useState([]);
+  const [query, setQuery] = useState();
 
-    return (
-        <div className={MapStyles.map_overlap}>
-            
+  console.log(query);
+
+  const fetch = async () => {
+    const { data } = await axios.get("/countries.json");
+    console.log(data);
+    setCountries(data);
+  };
+
+  useEffect(() => {
+    fetch();
+  }, []);
+
+  const filteredCountries = countries.filter((country) =>
+    country.country?.toLowerCase().startsWith(query?.toLowerCase())
+  );
+
+  console.log(filteredCountries);
+
+  return (
+    <div className={MapStyles.map_overlap}>
+      <div className={MapStyles.search_container}>
+        <div>
+          <label htmlFor="country"></label>
+          <input
+            type="text"
+            name="country"
+            id="country"
+            onChange={(e) => setQuery(e.target.value)}
+            className={MapStyles.input}
+          />
         </div>
-    )
-}
 
-export default Map
+        {filteredCountries.length == 0 ? (
+          <div>
+            {countries.map((country, index) => (
+              <p key={index}>{country.country}</p>
+            ))}
+          </div>
+        ) : (
+          <>
+            <div className={MapStyles.country_name}>
+              {filteredCountries.map((country, index) => (
+                <div key={index}>
+                  <p>{country.country}</p>
+                  {filteredCountries.length < 5 && (
+                    <img
+                      src={country.flag}
+                      alt=""
+                      key={index}
+                      className={MapStyles.country_image}
+                    />
+                  )}
+                </div>
+              ))}
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Map;
